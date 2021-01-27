0bf020794098320c6866d5c7feb3cc0141c9c39f
Emmanuel Losch <emmanuellosch@live.de>
2021-01-27 22:30:01 +0100

    profile

--- src/quiz-app.js
+++ src/quiz-app.js
@@ -1,14 +1,1 @@
-var btnRome = document.getElementById('rome');
-var btnGreece  = document.getElementById('greece');
-var Rome = document.getElementById('rome');
-var Greece  = document.getElementById('greece');
 
-btnRome.addEventListener('click', function(){
-  Rome.className = ''; 
-  Greece.className = 'hidden';
-});
-
-btnGreece.addEventListener('click', function(){
-  Rome.className = '';
-  Greece.className = 'hidden';
-});
