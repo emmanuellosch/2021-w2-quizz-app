630d243c372adfa75c1d9cac50a23835357fb21f
Emmanuel Losch <emmanuellosch@live.de>
2021-02-03 16:56:51 +0100

    javascript vol.1

--- src/quiz-app.js
+++ src/quiz-app.js
@@ -1,1 +1,37 @@
+console.clear();
+
+/*
+
+const button = document.querySelector('#button');
+const answer = document.querySelector('#answer1');
+
+button.addEventListener('click', () => {
+answer.classList.toggle('hidden');
+});
+
+*/
+
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach(button => 
+    button.addEventListener('click', () => {
+        const answer = button.parentNode.querySelector('.answer');
+        answer.classList.toggle('hidden');
+        button.innerText = answer.classList.contains('hidden') ? 'Show Answer' : 'Hide Answer';
+    })) 
+
+const bookmark = document.querySelectorAll('.bookmark');
+
+bookmark.forEach(bookmark =>
+    bookmark.addEventListener('click', () => {
+    bookmark.classList.toggle('fas')
+    bookmark.classList.toggle('active')
+    }))
+
+
+ 
+
+
+
+
 
